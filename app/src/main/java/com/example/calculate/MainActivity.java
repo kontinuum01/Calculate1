d3cd4dd5fc4a4b20506e177c9ad723fbc6f55a8b
Владислав <kontinuum01@gmail.com>
2021-09-01 22:12:36 +0300

    Commit

--- app/src/main/java/com/example/calculate/MainActivity.java
+++ app/src/main/java/com/example/calculate/MainActivity.java
@@ -3,12 +3,142 @@ package com.example.calculate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView Result;
+    private Button one, two, three, four, five, six, seven, eight, nine, zero, add, sub, div, mul, dot, C, AC;
+    private String input, answer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_grid);
+        setContentView(R.layout.activity_main);
+        Result = findViewById(R.id.Result);
+
+        one = findViewById(R.id.one);
+        two = findViewById(R.id.two);
+        three = findViewById(R.id.three);
+        four = findViewById(R.id.four);
+        five = findViewById(R.id.five);
+        six = findViewById(R.id.six);
+        seven = findViewById(R.id.seven);
+        eight = findViewById(R.id.eight);
+        nine = findViewById(R.id.nine);
+        zero = findViewById(R.id.zero);
+        add = findViewById(R.id.add);
+        sub = findViewById(R.id.sub);
+        div = findViewById(R.id.div);
+        mul = findViewById(R.id.mul);
+        dot = findViewById(R.id.dot);
+        C = findViewById(R.id.C);
+        AC = findViewById(R.id.AC);
+
+
+    }
+
+    public void ButtonClick(View view) {
+        Button button = (Button) view;
+        String data = button.getText().toString();
+        switch (data) {
+            case "AC":
+                input = "";
+                break;
+
+            case "*":
+                Solve();
+                input += "*";
+                break;
+            case "/":
+                Solve();
+                input += "/";
+                break;
+            case "=":
+                Solve();
+                answer = input;
+                break;
+            case "C":
+                String newText = input.substring(0, input.length() - 1);
+                input = newText;
+                break;
+            default:
+                if (input == null) {
+                    input = "";
+                }
+                if (data.equals("+") || data.equals("-") || data.equals("/")) {
+                    Solve();
+                }
+                input += data;
+
+        }
+        Result.setText(input);
+    }
+
+    private void Solve() {
+        if (input.split("\\*").length == 2) {
+            String number[] = input.split("\\*");
+            try {
+                double mul = Double.parseDouble(number[0]) * Double.parseDouble(number[1]);
+                input = mul + "";
+            } catch (Exception e) {
+
+            }
+        } else if (input.split("/").length > 2) {
+            String number[] = input.split("\\*");
+            try {
+                double div = Double.parseDouble(number[0]) / Double.parseDouble(number[1]);
+                input = div + "";
+            } catch (Exception e) {
+            }
+        } else if (input.split("^").length > 2) {
+            String number[] = input.split("\\^");
+            try {
+                double pow = Math.pow(Double.parseDouble(number[0]), Double.parseDouble(number[1]));
+                input = pow + "";
+            } catch (Exception e) {
+            }
+        } else if (input.split("\\+").length > 2) {
+            String number[] = input.split("\\+");
+            try {
+                double sum = Double.parseDouble(number[0]) + Double.parseDouble(number[1]);
+                input = sum + "";
+            } catch (Exception e) {
+            }
+        } else if (input.split("-").length > 1) {
+            String number[] = input.split("-");
+            if (number[0] == "" && number.length == 2) {
+                number[0] = 0 + "";
+            }
+            try {
+                double sub = 0;
+                if (number.length == 2) {
+                    sub = Double.parseDouble(number[0]) - Double.parseDouble(number[1]);
+                } else if (number.length == 3) {
+                    sub = Double.parseDouble(number[1]) - Double.parseDouble(number[2]);
+                }
+                input = sub + "";
+            } catch (Exception e) {
+            }
+        }
+        String n[] = input.split("\\.");
+        if (n.length > 1) {
+            if (n[1].equals("0")) {
+                input = n[0];
+            }
+        }
+        Result.setText(input);
     }
 }
+
+
+
+
+
+
+
+
+
